0de6e176edaf9262dc96dfe06319fc2470ab221c
Azzazeru <aaron.fuentesdioca@gmail.com>
2024-05-30 11:04:42 -0400

    Serializer Fixed

--- api/serializer.py
+++ api/serializer.py
@@ -1,5 +1,3 @@
-# myapp/serializers.py
-
 from rest_framework import serializers
 from .models import *
 
@@ -19,20 +17,30 @@ class ArtistaSerializer(serializers.ModelSerializer):
         fields = ['id_artista', 'nombre']
 
 class DetalleDiscoSerializer(serializers.ModelSerializer):
-    sello_discografico = SelloDiscograficoSerializer()
-    genero_musical = GeneroMusicalSerializer()
-    artista = ArtistaSerializer()
+    sello_discografico_id = serializers.PrimaryKeyRelatedField(queryset=SelloDiscografico.objects.all(), source='sello_discografico', write_only=True)
+    genero_musical_id = serializers.PrimaryKeyRelatedField(queryset=GeneroMusical.objects.all(), source='genero_musical', write_only=True)
+    artista_id = serializers.PrimaryKeyRelatedField(queryset=Artista.objects.all(), source='artista', write_only=True)
+    
+    sello_discografico = SelloDiscograficoSerializer(read_only=True)
+    genero_musical = GeneroMusicalSerializer(read_only=True)
+    artista = ArtistaSerializer(read_only=True)
 
     class Meta:
         model = DetalleDisco
-        fields = ['id_detalle_disco', 'sello_discografico', 'genero_musical', 'artista']
+        fields = [
+            'id_detalle_disco',
+            'sello_discografico', 'sello_discografico_id',
+            'genero_musical', 'genero_musical_id',
+            'artista', 'artista_id'
+        ]
 
 class DiscoSerializer(serializers.ModelSerializer):
-    detalle_disco = DetalleDiscoSerializer()
+    detalle_disco_id = serializers.PrimaryKeyRelatedField(queryset=DetalleDisco.objects.all(), source='detalle_disco', write_only=True)
+    detalle_disco = DetalleDiscoSerializer(read_only=True)
 
     class Meta:
         model = Disco
-        fields = ['id_disco', 'titulo', 'detalle_disco']
+        fields = ['id_disco', 'titulo', 'detalle_disco', 'detalle_disco_id']
 
 class CancionSerializer(serializers.ModelSerializer):
     artista = ArtistaSerializer()
@@ -54,24 +62,36 @@ class MarcaSerializer(serializers.ModelSerializer):
         fields = ['id_marca', 'nombre']
 
 class DetalleInstrumentoSerializer(serializers.ModelSerializer):
-    tipo_instrumento = TipoInstrumentoSerializer()
-    marca = MarcaSerializer()
+    tipo_instrumento_id = serializers.PrimaryKeyRelatedField(queryset=TipoInstrumento.objects.all(), source='tipo_instrumento', write_only=True)
+    marca_id = serializers.PrimaryKeyRelatedField(queryset=Marca.objects.all(), source='marca', write_only=True)
+    
+    tipo_instrumento = TipoInstrumentoSerializer(read_only=True)
+    marca = MarcaSerializer(read_only=True)
 
     class Meta:
         model = DetalleInstrumento
-        fields = ['id_detalle_instrumento', 'descripcion', 'tipo_instrumento', 'marca']
+        fields = [
+            'id_detalle_instrumento',
+            'descripcion',
+            'tipo_instrumento', 'tipo_instrumento_id',
+            'marca', 'marca_id'
+        ]
 
 class InstrumentoSerializer(serializers.ModelSerializer):
-    detalle_instrumento = DetalleInstrumentoSerializer()
+    detalle_instrumento_id = serializers.PrimaryKeyRelatedField(queryset=DetalleInstrumento.objects.all(), source='detalle_instrumento', write_only=True)
+    detalle_instrumento = DetalleInstrumentoSerializer(read_only=True)
 
     class Meta:
         model = Instrumento
-        fields = ['id_instrumento', 'modelo', 'detalle_instrumento']
+        fields = ['id_instrumento', 'modelo', 'detalle_instrumento', 'detalle_instrumento_id']
+
 
 class ProductoSerializer(serializers.ModelSerializer):
-    disco = DiscoSerializer()
-    instrumento = InstrumentoSerializer()
+    disco_id = serializers.PrimaryKeyRelatedField(queryset=Disco.objects.all(), source='disco', write_only=True, allow_null=True, required=False)
+    instrumento_id = serializers.PrimaryKeyRelatedField(queryset=Instrumento.objects.all(), source='instrumento', write_only=True, allow_null=True, required=False)
+    disco = DiscoSerializer(read_only=True)
+    instrumento = InstrumentoSerializer(read_only=True)
 
     class Meta:
         model = Producto
-        fields = ['id_producto', 'disco', 'instrumento', 'precio', 'stock']
+        fields = ['id_producto', 'disco', 'disco_id', 'instrumento', 'instrumento_id', 'precio', 'stock']
---
+++ client/static/js/adminScript.js
@@ -0,0 +1,49 @@
+const url = "https://gmad.up.railway.app/api/"
+        const d = document,
+            $discosTable = d.querySelector('#discos-table tbody'),
+            $instrumentosTable = d.querySelector('#instrumentos-table tbody'),
+            $discoTemplate = d.getElementById('disco-template').content,
+            $instrumentoTemplate = d.getElementById('instrumento-template').content,
+            $discoFragment = d.createDocumentFragment(),
+            $instrumentoFragment = d.createDocumentFragment();
+
+        const getAll = async () => {
+            try {
+                let res = await axios.get(url + 'productos/') , json = await res.data;
+
+                console.log(json);
+                console.log(url)
+
+                json.forEach(p => {
+                    if (p.disco) {
+                        $discoTemplate.querySelector(".id").textContent = p.id_producto;
+                        $discoTemplate.querySelector(".precio").textContent = p.precio;
+                        $discoTemplate.querySelector(".stock").textContent = p.stock;
+                        $discoTemplate.querySelector(".titulo").textContent = p.disco.titulo;
+                        $discoTemplate.querySelector(".sello").textContent = p.disco.detalle_disco?.sello_discografico?.nombre;
+                        $discoTemplate.querySelector(".artista").textContent = p.disco.detalle_disco?.artista?.nombre;
+
+                        let $clone = d.importNode($discoTemplate, true);
+                        $discoFragment.appendChild($clone);
+                    } else if (p.instrumento) {
+                        $instrumentoTemplate.querySelector(".id").textContent = p.id_producto;
+                        $instrumentoTemplate.querySelector(".precio").textContent = p.precio;
+                        $instrumentoTemplate.querySelector(".stock").textContent = p.stock;
+                        $instrumentoTemplate.querySelector(".modelo").textContent = p.instrumento.modelo;
+                        $instrumentoTemplate.querySelector(".tipo").textContent = p.instrumento.detalle_instrumento?.tipo_instrumento?.tipo;
+                        $instrumentoTemplate.querySelector(".marca").textContent = p.instrumento.detalle_instrumento?.marca?.nombre;
+
+                        let $clone = d.importNode($instrumentoTemplate, true);
+                        $instrumentoFragment.appendChild($clone);
+                    }
+                });
+
+                $discosTable.appendChild($discoFragment);
+                $instrumentosTable.appendChild($instrumentoFragment);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+
+        d.addEventListener("DOMContentLoaded", getAll);
