18bea31d3508148b43feb3ef4eff6f669fedda3c
Azzazeru <aaron.fuentesdioca@gmail.com>
2024-06-26 00:56:45 -0400

    ?

--- galaxymusicdp/urls.py
+++ galaxymusicdp/urls.py
@@ -19,7 +19,6 @@ from django.urls import path , include
 
 from django.views.generic import RedirectView
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
