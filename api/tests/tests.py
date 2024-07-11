027947c09667e65aae74906302d76c3ff8936475
Azzazeru <aaron.fuentesdioca@gmail.com>
2024-07-10 21:24:51 -0400

    Arreglo de base de datos :p

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,16 +1,22 @@
 import requests
+import pytest
+from api.models import Producto, Disco
+from rest_framework.test import APIClient
+from django.urls import reverse
 
-ENDPOINT = "https://gmad.up.railway.app/api"
+# ENDPOINT = "https://gmad.up.railway.app/api"
+ENDPOINT = "http://127.0.0.1:8000/api"
 
-def create_task(payload):
-    return request.put(f"{ENDPOINT}/public/productos/")
-
-def test_can_call_endpoint():
+def test_endpoint():
     response = requests.get(f"{ENDPOINT}/public/productos/")
     assert response.status_code == 200
 
-def test_can_list_products():
-    for _ in range(3):
-        payload = new_task_payload()
-        create_task_response = create_task(payload)
-        assert = create_task_response.status_code == 200
+@pytest.mark.django_db
+def test_get_productos():    
+    response = requests.get(f"{ENDPOINT}/public/productos/")
+    data = response.json()
+    
+    assert response.status_code == 200
+    
+    assert data[0]['id_producto'] == 1
+
